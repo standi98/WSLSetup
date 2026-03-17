b6600efb41bb51876de791c9d33f750c1f59b78e
standi98 <standidmc@gmail.com>
2026-03-17 11:39:10 +0100

    Added latex setup.

--- WSLSetup.sh
+++ WSLSetup.sh
@@ -13,3 +13,11 @@ git config --global user.email "standidmc@gmail.com"
 
 # UV setup
 curl -LsSf https://astral.sh/uv/install.sh | sh
+
+
+# LaTeX setup
+read -p "Do you want to install LaTeX? (y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    sudo apt install texlive-full
+fi
