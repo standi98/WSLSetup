ee95c354f1f5c12c78c3679793deb9ef59f075da
standi98 <standidmc@gmail.com>
2026-03-17 11:34:18 +0100

    Added UV installation

--- WSLSetup.sh
+++ WSLSetup.sh
@@ -1,7 +1,15 @@
+# WSL Setup
 sudo apt update
+
+
+# Python setup
 sudo apt install python3-pip
 
 
 # Git setup
 git config --global user.name "standi98"
 git config --global user.email "standidmc@gmail.com"
+
+
+# UV setup
+curl -LsSf https://astral.sh/uv/install.sh | sh
